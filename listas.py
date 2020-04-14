07caca5019c455552a07aa435f1c79d8cce0b3c7
MT7715077 <megamegamegaomega@gmail.com>
2020-04-13 23:20:57 -0400

    FIN

---
+++ listas.py
@@ -0,0 +1,31 @@
+#listas
+colores = ["rojo", "amarillo", "verde"]
+colores
+print(colores)
+colores[0] = "Azul"
+print(colores)
+colores[2] = "violeta"
+print(colores)
+len(colores)
+print(colores)
+
+print(len(colores))
+
+colores.append("naranja")
+print(colores)
+
+colores.remove("amarillo")
+print(colores)
+
+
+for color in colores:
+    print(color)
+
+
+colores.clear()
+print(colores)    
+
+
+
+
+
