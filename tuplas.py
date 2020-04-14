0b397f66d403007a592cf8383932dfda35e9fa4e
MT7715077 <megamegamegaomega@gmail.com>
2020-04-13 23:32:02 -0400

    Se agregan info tuplas

---
+++ tuplas.py
@@ -0,0 +1,9 @@
+tupla_colores = ("rojo" , "verde" ,"amrillo")
+print(tupla_colores)
+
+for color in tupla_colores:
+    print(color)
+
+
+    # No se puede eliminar no agregar elementos
+print(len(tupla_colores))    
