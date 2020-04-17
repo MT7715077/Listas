0b8cf7a088a7aeb87b2c913ef44b9845b28cee89
MT7715077 <megamegamegaomega@gmail.com>
2020-04-17 01:29:50 -0400

    CONDICIONES

---
+++ condiciones.py
@@ -0,0 +1,26 @@
+a = 10
+b = 1
+c = 2
+d = 8
+
+if(a > b):
+    print("A es mayor a B")
+else:
+    print("B es mayor a A")
+
+
+if(a > b) and (b < d):
+    print("La exprecion es correcta !!")
+else:
+    print("La exprecion no es correcta")
+
+dia = "jueves"
+
+if(dia == "Lunes"):
+    print("Primer If")
+elif(dia == "Martes"):
+    print("segundo if")
+elif(dia == "jueves"):
+    print("CORRECTO,  es jueves")
+else:
+    print("No es ninguna correcta")
